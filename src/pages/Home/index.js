dc91919aa197ea1259626104271fea2195c82d5a
Daniel Silva <danielrodriguesdrs331@gmail.com>
2020-05-25 13:30:24 -0300

    NEW:Add style in home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles';
 
 function Home() {
     return (
-        <h1>Home</h1>
+        <ProductList>
+            <li>
+                <img src="https://images-submarino.b2w.io/produtos/01/00/img/59394/0/59394048_1GG.jpg" alt="Notebook" />
+                <strong>Notebook</strong>
+                <span>R$1.299,90</span>
+                <button>
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+        </ProductList>
     );
 }
 
