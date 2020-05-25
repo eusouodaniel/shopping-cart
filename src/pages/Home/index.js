46c299c2b23d326ee210cf901a990b391fab1ad0
Daniel Silva <danielrodriguesdrs331@gmail.com>
2020-05-25 14:57:57 -0300

    NEW: Search data from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md'
 import { ProductList } from './styles';
+import api from '../../services/api';
 
-function Home() {
-    return (
-        <ProductList>
-            <li>
-                <img
-                    src="https://images-submarino.b2w.io/produtos/01/00/img/59394/0/59394048_1GG.jpg"
-                    alt="Notebook"
-                />
-                <strong>Notebook</strong>
-                <span>R$1.299,90</span>
-                <button>
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" />
-                    </div>
-                    <span>Adicionar ao carrinho</span>
-                </button>
-            </li>
-        </ProductList>
-    );
+class Home extends Component {
+    state = {
+        products: []
+    };
+
+    async componentDidMount() {
+        const response = await api.get('products');
+        this.setState({
+            products: response.data
+        });
+    }
+
+    render() {
+        const { products } = this.state;
+
+        return (
+            <ProductList>
+                { products.map(product => (
+                    <li key={product.id}>
+                        <img
+                            src={product.image}
+                            alt={product.title}
+                        />
+                        <strong>{product.title}</strong>
+                        <span>{product.price}</span>
+                        <button>
+                            <div>
+                                <MdAddShoppingCart size={16} color="#FFF" />
+                            </div>
+                            <span>Adicionar ao carrinho</span>
+                        </button>
+                    </li>
+                )) }
+
+            </ProductList>
+        );
+    }
 }
 
 export default Home;
